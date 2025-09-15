916eb84ef868319fc75bbc098b4c65672ccd20ae
wuhuangjianCN <95284379+wuhuangjianCN@users.noreply.github.com>
2025-09-15 15:39:57 +0800

    Delete ProNet_allInOne directory

--- ProNet_allInOne/calLossSeries.m
+++
@@ -1,7 +0,0 @@
-function lossSeries=calLossSeries(DSinAstart,analysis_aStart,gridWeight_loss)
-    tmp=nan(size(DSinAstart,5),1);
-    for n_tmp=1:numel(tmp)
-        tmp(n_tmp)=mean(abs(DSinAstart(:,:,:,:,n_tmp)-analysis_aStart(:,:,:,:,n_tmp)).*gridWeight_loss,'all');
-    end
-    lossSeries=numel(gridWeight_loss)/sum(gridWeight_loss,'all').*tmp;
-end
--- ProNet_allInOne/modelGradients_balance.m
+++
@@ -1,6 +0,0 @@
-function [gradients,state,loss,dlY] = modelGradients_balance(dlX,dlY_target,parameters,state,weight)
-
-    [dlY,state] = model_balance(dlX,parameters,state);
-    loss = partialLoss(dlY,dlY_target,weight);
-    gradients = dlgradient(loss,parameters);
-end
